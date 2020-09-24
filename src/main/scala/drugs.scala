c2d429c5172477c500914d3ea0dd75b4d89b0fe3
Laura Kreile <laura.kreile@inbox.lv>
2020-09-24 20:22:05 +0300

    First commit

--- src/main/scala/drugs.scala
+++ src/main/scala/drugs.scala
@@ -17,33 +17,26 @@ object final_work_1 extends App {
 
   }
   //myText.foreach(println) // pagaidām varbūt neprintējam visu tekstu, lai vieglāk redzēt izdarītos/atlasītos (?)
+  // šo noteikti ņemsim nost, es pagaidām uzliku lai skatītos tekstu
+
   val longestLine = myText.sortBy(_.size).reverse(0) //the longest line in text
   println(s"In the text the longest line is $longestLine")
 
-  val longestLineSize = longestLine.size
-  println(s"In the text the longest line amount is $longestLineSize") //the longest line amount of letter
+  val longestLineLength = longestLine.length
+  println(s"In the text the longest line amount is $longestLineLength") //the longest line amount of symbol
 
-  val longestWord = myText.map(word => word -> word.length)
-  println(longestWord.max, longestWord.min) // japārbauda ! // izskatās, ka pagaidām neuzrādās īsākais vārds - rezultātos tukšums
+
+  val splitLine = longestLine.split(" ") //to split the words(symbols as well) length
+  val longestWord = splitLine.map(word => word ->  word.length) //sim smuki sanak visi vardi ar garuma skaitu no
+  longestWord.foreach(println)                                  //tikai vel nekadi nevaru atrast pareizo formulejumu
+                                                                // max un min, bet gan to atkodisu :-)
+
+  println(longestWord.max, longestWord.min) //īsti nav pareizs!!!
+
+
+  //println(longestWord.max, longestWord.min) // japārbauda ! // izskatās, ka pagaidām neuzrādās īsākais vārds - rezultātos tukšums
   // /arī garākais vārds izskatās, ka sastāv no 2 vārdiem.
   // TODO Mēģināsim atrisināt šo "longestWord" ...
 
-  //println(myLineSplits.max, myLineSplits.min)
-  //val longestLineSum = myText.map (txt => txt(longestLine.length)).toSeq //longest line letters sum
-  //println(longestLineSum)
 
-
-  //  myLines.split("\\D+").filter(_.nonEmpty).toList
-  //  print(("""\d+""".r findAllIn myText).toList)
-
-  //  myText.split(" ").filter(_.exists(num))
-  //  (" ").filter(_.Null).toList
-
-
-  //  def saveSeq(destName:String, mySeq:Seq[String]) = {
-  //    println(s"Saving my Sequence to file $destName")
-  //  }
-  //  val mySeq = srcName
-  //  //val filteredSeq = processSeq(mySeq)
-  //  saveSeq(dstName)
 }
